d9de0a95de2a030873933bca4c0ec147078425e1
Alex Kalinichenko <aleks.kalinichenko989@gmail.com>
2026-09-09 17:05:08 +0300

    feat: commit all code for test simple Hello world

--- cmd/get-item/main.go
+++ cmd/get-item/main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"arizona-server/internal/db"
 	"arizona-server/internal/service"
 	"fmt"
-	"log"
 	"net/http"
 )
 
 var gItemService *service.Service
 
 func init() {
-	log.Println("Function step 0: init func get item")
-	dbClient, err := db.ConnectionToYDB()
-	if err != nil {
-		fmt.Printf("DB connection error: %v\n", err)
-		return
-	}
-	gItemService = service.NewService(dbClient)
+	//log.Println("Function step 0: init func get item")
+	//dbClient, err := db.ConnectionToYDB()
+	//if err != nil {
+	//	fmt.Printf("DB connection error: %v\n", err)
+	//	return
+	//}
+	//gItemService = service.NewService(dbClient)
 }
 
 func GetItem(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Function step 1")
-	if gItemService == nil {
-		fmt.Println("[ERROR] Service not initialized, returning 500")
-		http.Error(w, `{"error":"db not initialized"}`, http.StatusInternalServerError)
-		return
-	}
-	gItemService.GetItem(r.Context())
+	fmt.Println("[OK] Hello")
+	w.Write([]byte("OK"))
+	//log.Printf("Function step 1")
+	//if gItemService == nil {
+	//	fmt.Println("[ERROR] Service not initialized, returning 500")
+	//	http.Error(w, `{"error":"db not initialized"}`, http.StatusInternalServerError)
+	//	return
+	//}
+	//gItemService.GetItem(r.Context())
 }
 func main() {
 
