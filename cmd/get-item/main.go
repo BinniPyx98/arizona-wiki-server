39e9546158ffea6324060a905bbffda749583a4f
Alex Kalinichenko <aleks.kalinichenko989@gmail.com>
2026-09-09 16:20:28 +0300

    feat: change path to db key for yfunction

--- cmd/get-item/main.go
+++ cmd/get-item/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"arizona-server/internal/db"
 	"arizona-server/internal/service"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,14 +11,23 @@ import (
 var gItemService *service.Service
 
 func init() {
-	dbClient := db.ConnectionToYDB()
+	log.Println("Function step 0: init func get item")
+	dbClient, err := db.ConnectionToYDB()
+	if err != nil {
+		fmt.Printf("DB connection error: %v\n", err)
+		return
+	}
 	gItemService = service.NewService(dbClient)
 }
 
 func GetItem(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Function step 1")
-	ctx := r.Context()
-	gItemService.GetItem(ctx)
+	if gItemService == nil {
+		fmt.Println("[ERROR] Service not initialized, returning 500")
+		http.Error(w, `{"error":"db not initialized"}`, http.StatusInternalServerError)
+		return
+	}
+	gItemService.GetItem(r.Context())
 }
 func main() {
 
--- internal/db/database.go
+++ internal/db/database.go
@@ -2,26 +2,39 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"cloud.google.com/go/firestore"
 	"google.golang.org/api/option"
 )
 
-func ConnectionToYDB() *firestore.Client {
+func ConnectionToYDB() (*firestore.Client, error) {
 	//Connect to db
 	ctx := context.Background()
 
 	projectID := "arizona-wiki"
-	pathToKey := "C:\\Users\\aleks\\Desktop\\GO_Projects\\Arizona_wiki_server\\internal\\db\\firebase_key.json"
+	// Path to key for local test
+	// pathToKey := "C:\\Users\\aleks\\Desktop\\GO_Projects\\Arizona_wiki_server\\internal\\db\\firebase_key.json"
+	//opt := option.WithAuthCredentialsFile(option.ServiceAccount, pathToKey)
 
-	opt := option.WithAuthCredentialsFile(option.ServiceAccount, pathToKey)
+	keyJSON := os.Getenv("DATABASE_KEY")
+	if keyJSON == "" {
+		return nil, fmt.Errorf("Database key is empty")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	opt := option.WithCredentialsJSON([]byte(keyJSON))
 
 	client, err := firestore.NewClient(ctx, projectID, opt)
 
 	if err != nil {
-		log.Printf("error creating client: %v", err)
+		return nil, fmt.Errorf("error creating client: %v", err)
 	}
 	log.Println("Connected to YDB")
-	return client
+	return client, nil
 }
